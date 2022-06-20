4365bc38e0a0313b59a8298b341226acb51a39aa
svasenkov <aanher@gmail.com>
2022-06-20 21:34:20 +0300

    added demoqa form test

---
+++ src/test/java/tests/demoqa/TestBase.java
@@ -0,0 +1,17 @@
+package tests.demoqa;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
+
+public class TestBase {
+
+    @BeforeAll
+    static void setUp() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+    }
+}
